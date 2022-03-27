6ab4acb45023ae6a70691f5359329434c4db23e2
mike-freislich <mike.freislich@gmail.com>
2022-03-27 13:11:58 +0200

    turrets

--- Assets/Bomb/Bomb.cs
+++ Assets/Bomb/Bomb.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-
     public GameObject explosion;
     public float accelerationDelay = 0.3f;
     public float acceleration = 0.9f;
     public float maxVelocity = 1.0f;
-
     float speed = 0;
     float delayTimer = 0;
 
     void Start()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
-        rb.velocity = new Vector3(2,-2,0);
+        rb.velocity = new Vector3(2, -2, 0);
     }
 
     void Update()
@@ -36,12 +34,21 @@ public class Bomb : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 7)
-        {                        
-            GameObject explodeObject = Instantiate<GameObject>(explosion, transform.position, transform.rotation);
-            Destroy(gameObject);
-            Destroy(explodeObject, 2);
+        switch (other.gameObject.layer)
+        {
+            case 7: Explode(); break;
+            case 9: 
+                Enemy enemy = other.GetComponent<Enemy>();
+                enemy.TakeDamage(100);
+                Explode();
+                break;
         }
     }
 
+    void Explode()
+    {
+        GameObject explodeObject = Instantiate<GameObject>(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Destroy(explodeObject, 2);
+    }
 }
