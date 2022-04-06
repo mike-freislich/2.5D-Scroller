e987ca9a0cfe8c65c71321ee5b00c35608ca6c63
Mike Freislich <mike.freislich@gmail.com>
2022-04-06 20:07:02 +0200

    Ray Casting step 1

--- Assets/GUI/application.cs
+++ Assets/GUI/application.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum QualityLevel { VeryLow, Low, Medium, High, VeryHigh, Ultra }
+enum GameLayer {
+    Default, TransparentFX, IgnoreRayCast, Water, UI, PlayerBullets, Platform, Player, Enemy
+}
+
 public class application : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 24;
-        //QualitySettings.vSyncCount = 1;
-        //QualitySettings.SetQualityLevel(0);
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+        QualitySettings.SetQualityLevel((int)QualityLevel.VeryHigh);
         
     }
 
