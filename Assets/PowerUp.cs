a7c74b94a48df44e2b5914cfd3e65d1e74732d8a
Mike Freislich <mike.freislich@gmail.com>
2022-04-08 20:40:27 +0200

    powerup lighting

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class PowerUp : MonoBehaviour
 {
@@ -9,7 +10,16 @@ public class PowerUp : MonoBehaviour
 
     void Start()
     {
-        
+        GameObject light = transform.Find("Light").gameObject;
+        GameObject halo = transform.Find("Halo").gameObject;
+        light.GetComponent<Light>().color = color;
+        halo.GetComponent<Light>().color = color;
+
+        UnityEditor.SerializedObject debugLight = new UnityEditor.SerializedObject(light);
+        debugLight.FindProperty("color").colorValue = color;
+        UnityEditor.SerializedObject debugHalo = new UnityEditor.SerializedObject(halo);
+        debugLight.FindProperty("color").colorValue = color;
+
     }
         
     void Update()
